f9f233150ee33e6039bbc40738edeca249dd64c9
typohydra <74554736+typohydra@users.noreply.github.com>
2023-01-06 13:30:50 +0400

    simple end to end tests 11.9 fix lint

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,19 +1,19 @@
-describe("Pokedex", function () {
-  it("front page can be opened", function () {
-    cy.visit("http://localhost:5000");
-    cy.contains("ivysaur");
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
-  });
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
 
-  it("Pokemon page can be navigated to", function () {
-    cy.visit("http://localhost:5000");
-    cy.get("a").eq(4).click()
-    cy.contains("charmeleon");
-    cy.contains("speed");
+  it('Pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('a').eq(4).click()
+    cy.contains('charmeleon')
+    cy.contains('speed')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
-  });
-});
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+})
